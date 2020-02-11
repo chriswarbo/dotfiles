938c5133209f31ca463b6a10e01196aa2d7fa3c1
Chris Warburton <chris@zipabout.com>
2020-02-11 09:06:03 +0000

    Hard-code a bunch more systemPackages and fonts

--- nixpkgs/darwin-configuration.nix
+++ nixpkgs/darwin-configuration.nix
@@ -3,14 +3,39 @@
 {
   # List packages installed in system profile. To search by name, run:
   # $ nix-env -qaP | grep wget
-  environment.systemPackages = [
-    pkgs.git
-    pkgs.vim
+  environment.systemPackages = with pkgs; [
+    awscli
+    docker
+    emacs
+    #firefox
+    gcc
+    git
+    #mysql-workbench
+    openjdk
+    #postman
+    sbt
+    vim
   ];
 
   # Use a custom configuration.nix location.
   # $ darwin-rebuild switch -I darwin-config=$HOME/.config/nixpkgs/darwin/configuration.nix
   # environment.darwinConfig = "$HOME/.config/nixpkgs/darwin/configuration.nix";
+
+  fonts = {
+    #enableDefaultFonts      = true;
+    /*fontconfig.defaultFonts = {
+      monospace = [ "Droid Sans Mono" ];
+      sansSerif = [ "Droid Sans"      ];
+      serif     = [ "Droid Sans"      ];
+    };*/
+    fonts = [
+      pkgs.anonymousPro
+      #pkgs.droid-fonts
+      pkgs.liberation_ttf
+      pkgs.terminus_font
+      pkgs.ttf_bitstream_vera
+    ];
+  };
 
   # Auto upgrade nix package and the daemon service.
   services.nix-daemon.enable = true;
