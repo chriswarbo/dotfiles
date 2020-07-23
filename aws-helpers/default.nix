28bf1370b20f28cbf45a12479c8c4ddabf86b8f7
Chris Warburton <chris@zipabout.com>
2020-07-23 11:13:18 +0100

    Alphabetical order

--- aws-helpers/default.nix
+++ aws-helpers/default.nix
@@ -1,18 +1,18 @@
 { awscli, bash, buildEnv, jq, lib, mkBin }:
 
 rec {
-  login = mkBin rec {
-    name  = "aws-login";
-    paths = [ bash awscli ];
-    file  = ./login.sh;
-  };
-
   api-info = mkBin rec {
     name  = "api-info";
     paths = [ bash awscli jq ];
     file  = ./api-info.sh;
   };
 
+
+  login = mkBin rec {
+    name  = "aws-login";
+    paths = [ bash awscli ];
+    file  = ./login.sh;
+  };
   combined = buildEnv {
     name  = "aws-helpers";
     paths = [ api-info login ];
