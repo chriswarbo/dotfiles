3b0e42e74ec2b2318ec9fbf5a23434dd016f171c
Chris Warburton <chris@zipabout.com>
2020-07-23 13:41:14 +0100

    We're not using recursion

--- aws-helpers/default.nix
+++ aws-helpers/default.nix
@@ -1,19 +1,19 @@
 { awscli, bash, buildEnv, jq, lib, mkBin }:
 
 rec {
-  api-info = mkBin rec {
+  api-info = mkBin {
     name  = "api-info";
     paths = [ bash awscli jq ];
     file  = ./api-info.sh;
   };
 
-  healthcheck = mkBin rec {
+  healthcheck = mkBin {
     name  = "healthcheck";
     paths = [ api-info bash awscli jq ];
     file  = ./healthcheck.sh;
   };
 
-  login = mkBin rec {
+  login = mkBin {
     name  = "aws-login";
     paths = [ bash awscli ];
     file  = ./login.sh;
